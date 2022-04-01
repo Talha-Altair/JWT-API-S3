86c5e96e685ff0060b3e5a2ce02277009d5ae678
Talha-Altair <cooltalha27@gmail.com>
2022-04-01 21:09:33 +0530

    Updates for 2022-04-01 21:09:33

--- connections.py
+++ connections.py
@@ -4,4 +4,3 @@ import settings
 s3 = boto3.resource('s3',
                     aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
                     aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,)
-
--- settings.py
+++ settings.py
@@ -4,6 +4,6 @@ import os
 
 load_dotenv()
 
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-BUCKET_NAME = "altair-bucket"
+AWS_ACCESS_KEY_ID       = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY   = os.getenv("AWS_SECRET_ACCESS_KEY")
+BUCKET_NAME             = "altair-bucket"
