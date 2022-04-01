6afab51ccb42d40c88ff48045e38cf7579fe599a
Talha-Altair <cooltalha27@gmail.com>
2022-04-01 19:55:10 +0530

    Updates for 2022-04-01 19:55:10

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, JWTManager
+
 import uuid
 
 app = Flask(__name__)
 
+app.config["JWT_SECRET_KEY"] = "haadha ba3da ka5eer"
+
+jwt = JWTManager(app)
+
 
 @app.route('/ping')
+@jwt_required()
 def ping():
 
     return jsonify({'ping': 'pong'})
@@ -13,7 +20,22 @@ def ping():
 @app.route('/')
 def home():
 
-    return jsonify({'routes': ['/ping', '/create', '/read', '/update', '/delete']})
+    return jsonify({'routes': ['/ping', '/login', '/create', '/read', '/update', '/delete']})
+
+
+@app.route("/login", methods=["POST"])
+def login():
+
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+
+    if username != "altair" or password != "1234":
+
+        return jsonify({"Error": "Incorrect "}), 401
+
+    access_token = create_access_token(identity=username)
+
+    return jsonify(access_token=access_token)
 
 
 if __name__ == '__main__':
